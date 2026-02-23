05b973d7b53096c3a2003c00c74b0ad3bf2dfbeb
Chinonso Beluonwu <mensahekwo@gmail.com>
2026-02-23 20:37:18 +0000

    Lower vals than expected

--- services/idw.py
+++ services/idw.py
@@ -2,14 +2,14 @@ import math
 
 # haversine function is used to calculate distance between two points
 def haversine(lat1, lon1, lat2, lon2):
-    R = 6371  # radius of earth km
-    radLat1, radLat2 = math.radians(lat1), math.radians(lat2) # convert to radians
-    diffLat = math.radians(lat2 - lat1) #convert differences to radians
-    diffLon = math.radians(lon2 - lon1)
-    # encodes distance between points using trigonometry
-    a = math.sin(diffLat / 2)**2 + math.cos(radLat1) * math.cos(radLat2) * math.sin(diffLon / 2)**2
+    R = 6371  # km
+    phi1, phi2 = math.radians(lat1), math.radians(lat2)
+    dphi = math.radians(lat2 - lat1)
+    dlambda = math.radians(lon2 - lon1)
 
-    # convert into usable distance in km
+    a = math.sin(dphi / 2)**2 + \
+        math.cos(phi1) * math.cos(phi2) * math.sin(dlambda / 2)**2
+
     return 2 * R * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
 # idw computation
