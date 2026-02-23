6f509b8c3b6b48a1e525fcc9efbfd5af116d57d0
Chinonso Beluonwu <mensahekwo@gmail.com>
2026-02-23 20:32:17 +0000

    Change

--- services/idw.py
+++ services/idw.py
@@ -1,14 +1,15 @@
 import math
 
+# haversine function is used to calculate distance between two points
 def haversine(lat1, lon1, lat2, lon2):
-    R = 6371  # km
-    phi1, phi2 = math.radians(lat1), math.radians(lat2)
-    dphi = math.radians(lat2 - lat1)
-    dlambda = math.radians(lon2 - lon1)
+    R = 6371  # radius of earth km
+    radLat1, radLat2 = math.radians(lat1), math.radians(lat2) # convert to radians
+    diffLat = math.radians(lat2 - lat1) #convert differences to radians
+    diffLon = math.radians(lon2 - lon1)
+    # encodes distance between points using trigonometry
+    a = math.sin(diffLat / 2)**2 + math.cos(radLat1) * math.cos(radLat2) * math.sin(diffLon / 2)**2
 
-    a = math.sin(dphi / 2)**2 + \
-        math.cos(phi1) * math.cos(phi2) * math.sin(dlambda / 2)**2
-
+    # convert into usable distance in km
     return 2 * R * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
 # idw computation
@@ -17,7 +18,7 @@ def idw(lat, lon, stations, power=2):
     denominator = 0
 
     for s in stations:
-        d = haversine(lat, lon, s["lat"], s["lon"])
+        d = haversine(lat, lon, s["lat"], s["lon"]) # uses haversine function to calculate distance between the current route point and  the WAQI station
         if d == 0:
             return s["aqi"]
 
