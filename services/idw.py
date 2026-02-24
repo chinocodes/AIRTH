fe7a4adfa48285d2c20c32294cb16e26c39f976d
Chinonso Beluonwu <mensahekwo@gmail.com>
2026-02-24 15:11:57 +0000

    Make haversine function variable names more intuitive

--- services/idw.py
+++ services/idw.py
@@ -3,12 +3,11 @@ import math
 # haversine function is used to calculate distance between two points
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371  # km
-    phi1, phi2 = math.radians(lat1), math.radians(lat2)
+    rad_lat1, rad_lat2 = math.radians(lat1), math.radians(lat2)
     dphi = math.radians(lat2 - lat1)
-    dlambda = math.radians(lon2 - lon1)
+    rad_diff_lon = math.radians(lon2 - lon1)
 
-    a = math.sin(dphi / 2)**2 + \
-        math.cos(phi1) * math.cos(phi2) * math.sin(dlambda / 2)**2
+    a = math.sin(dphi / 2)**2 + math.cos(rad_lat1) * math.cos(rad_lat2) * math.sin(rad_diff_lon / 2)**2
 
     return 2 * R * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
