c30f7b3a871e2d28ae82e5edeab1589c51367d50
Chinonso Beluonwu <mensahekwo@gmail.com>
2026-02-24 15:18:01 +0000

    more intuition

--- services/idw.py
+++ services/idw.py
@@ -4,10 +4,10 @@ import math
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371  # km
     rad_lat1, rad_lat2 = math.radians(lat1), math.radians(lat2)
-    dphi = math.radians(lat2 - lat1)
+    rad_diff_lat = math.radians(lat2 - lat1)
     rad_diff_lon = math.radians(lon2 - lon1)
 
-    a = math.sin(dphi / 2)**2 + math.cos(rad_lat1) * math.cos(rad_lat2) * math.sin(rad_diff_lon / 2)**2
+    a = math.sin(rad_diff_lat / 2)**2 + math.cos(rad_lat1) * math.cos(rad_lat2) * math.sin(rad_diff_lon / 2)**2
 
     return 2 * R * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
